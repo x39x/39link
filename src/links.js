0304d0a7ecc9669cd5fd6efbcac16168baa552ac
Y.S <vv.ys.ma@gmail.com>
2025-04-01 19:46:27 +0800

    Tue Apr  1 19:46:27 CST 2025 add links

--- src/links.js
+++ src/links.js
@@ -3,30 +3,31 @@ export const links = {};
 
 links["常用"] = [
     { url: "https://dash.cloudflare.com", dsc: "Cloudflare Dashboard" },
+    { url: "https://www.codewars.com", dsc: "CodeWars" },
 ];
 
-links["AI"] = [
+links["TODO"] = [
     {
-        url: "https://chat.openai.com",
-        dsc: "OpenAI",
+        url: "https://www.topgoer.com",
+        dsc: "Go语言中文文档",
     },
-    { url: "https://www.midjourney.com", dsc: "Midjourney" },
+    {
+        url: "https://jvns.ca/blog/2024/02/16/popular-git-config-options/",
+        dsc: "Popular git config options",
+    },
+    {
+        url: "https://nju-projectn.github.io/ics-pa-gitbook/ics2024/index.html",
+        dsc: "南京大学 计算机系统基础 ",
+    },
 ];
 
-links["Net"] = [
-    {
-        url: "https://ipleak.net",
-        dsc: "IPLeak",
-    },
-    {
-        url: "https://proxy.incolumitas.com/proxy_detect.html",
-        dsc: "Live Proxy and VPN Detection",
-    },
-    {
-        url: "https://proxy.888005.xyz",
-        dsc: "Proxy 检测",
-    },
+links["动漫 音乐 电影"] = [
+    { url: "https://741dm.com", dsc: "樱花动漫" },
+    { url: "https://www.hifini.com", dsc: "HiFINI 无损音乐下载" },
+    { url: "https://www.bugutv.vip", dsc: "布谷TV 4k影视资源" },
+    { url: "https://zimuku.org", dsc: "字幕" },
 ];
+
 links["漫画"] = [
     {
         url: "https://mox.moe",
@@ -38,7 +39,36 @@ links["漫画"] = [
     },
 ];
 
-links["Blog Gallery"] = [
+links["Tools"] = [
+    {
+        url: "https://downgit.evecalm.com",
+        dsc: "DownGit",
+    },
+    {
+        url: "https://transform.tools",
+        dsc: "代码、配置类文件转换",
+    },
+];
+
+links["画图 图片处理"] = [
+    { url: "https://iloveimg.com/zh-cn", dsc: "在线图片编辑器" },
+    { url: "https://excalidraw.com", dsc: "Excalidraw" },
+    { url: "https://tldraw.com", dsc: "tldraw" },
+    { url: "https://bilibiliq.com", dsc: "bilibili 封面提取" },
+];
+
+links["设计"] = [
+    {
+        url: "https://www.pinterest.com",
+        dsc: "Pinterest",
+    },
+    {
+        url: "https://fontiko.com",
+        dsc: "Fontiko",
+    },
+];
+
+links["Web Gallery"] = [
     {
         url: "https://itsbruno.dev",
         dsc: "Bruno",
@@ -53,9 +83,102 @@ links["Blog Gallery"] = [
     },
 ];
 
+links["Mac 软件下载"] = [
+    {
+        url: "https://appstorrent.ru",
+        dsc: "appstorrent",
+    },
+    {
+        url: "https://www.macat.vip",
+        dsc: "马克喵",
+    },
+    {
+        url: "https://macwk.com.cn",
+        dsc: "MacVK",
+    },
+    {
+        url: "https://foxirj.com",
+        dsc: "佛系软件",
+    },
+    {
+        url: "https://macapps.pro",
+        dsc: "Mac Apps Pro",
+    },
+    {
+        url: "https://macked.app",
+        dsc: "MacKed",
+    },
+    {
+        url: "https://xclient.info",
+        dsc: "xclient",
+    },
+];
+
+links["C++"] = [
+    { url: "https://www.learncpp.com", dsc: "LEARN C++" },
+    { url: "https://en.cppreference.com/", dsc: "Cpp Reference" },
+    {
+        url: "https://zhuanlan.zhihu.com/p/688090551",
+        dsc: "C++11、C++14、C++17、C++20新特性总结",
+    },
+    {
+        url: "https://windsting.github.io/a-tour-of-cpp-2nd-cn/ch01.html",
+        dsc: "A Tour of C++ (Second edition) 中译",
+    },
+    {
+        url: "https://github.com/Cpp-Club/Cxx_HOPL4_zh",
+        dsc: " Bjarne Stroustrup HOPL4 论文",
+    },
+    { url: "https://oi-wiki.org", dsc: "OI Wiki" },
+];
+
+links["其他"] = [
+    {
+        url: "https://whycan.com",
+        dsc: "哇酷开发者社区(硬件)",
+    },
+    {
+        url: "https://react-ui-libraries.vercel.app",
+        dsc: "React UI libraries",
+    },
+];
+
 links["Miku"] = [
     {
         url: "https://piapro.net/intl/zh-cn_character.html",
         dsc: "关于Piapro的角色们",
     },
+];
+
+links["Ip DNS Proxy"] = [
+    {
+        url: "https://ip111.cn",
+        dsc: "IP111",
+    },
+    {
+        url: "https://ipleak.net",
+        dsc: "IPLeak",
+    },
+    {
+        url: "https://proxy.incolumitas.com/proxy_detect.html",
+        dsc: "Live Proxy and VPN Detection",
+    },
+    {
+        url: "https://proxy.888005.xyz",
+        dsc: "Proxy 检测",
+    },
+];
+
+links["Math"] = [
+    {
+        url: "https://typst.app/docs/reference/symbols/sym/",
+        dsc: "Typst Symbols",
+    },
+    { url: "https://kz16.top/latex/symbol/", dsc: "latex常用符号" },
+    { url: "https://www.latexstudio.net", dsc: "LaTeX 工作室" },
+];
+
+links["支付 注册"] = [
+    { url: "https://bewildcard.com", dsc: "WildCard" },
+    { url: "https://sms-activate.guru/cn", dsc: "电话卡" },
 ];
